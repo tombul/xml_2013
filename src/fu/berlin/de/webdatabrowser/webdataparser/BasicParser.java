85292d46d735a90f752b56bdb9d7774f0bf8ce8b
tombul <tombullmann@googlemail.com>
2013-06-18 17:19:50 +0200

    changed AndroidHttpClient back to HttpClient

--- src/fu/berlin/de/webdatabrowser/webdataparser/BasicParser.java
+++ src/fu/berlin/de/webdatabrowser/webdataparser/BasicParser.java
@@ -4,16 +4,21 @@
 package fu.berlin.de.webdatabrowser.webdataparser;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 
 /**
@@ -39,19 +44,20 @@ public class BasicParser {
 
     private String getHttpResponseString(String url, String agent) {
         String defaultAgent = "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
-        AndroidHttpClient client = null;
-        if(!(agent == null || agent.isEmpty())) {
-            client = AndroidHttpClient.newInstance(agent);
-        }
-        else {
-            client = AndroidHttpClient.newInstance(defaultAgent);
-        }
+        // AndroidHttpClient client = null;
+        // if(!(agent == null || agent.length() == 0)) {
+        // client = AndroidHttpClient.newInstance(agent);
+        // }
+        // else {
+        // client = AndroidHttpClient.newInstance(defaultAgent);
+        // }
+        HttpClient client = new DefaultHttpClient();
         HttpGet request = new HttpGet(url);
         String html = "";
 
         try {
             HttpResponse response = client.execute(request);
-            client.close();
+            // client.close();
             InputStream inputStream = response.getEntity().getContent();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream(16);
             byte[] buffer = new byte[16];
@@ -62,6 +68,7 @@ public class BasicParser {
             }
 
             html = outputStream.toString();
+            Log.v("asynkBlaHtml", html);
             inputStream.close();
             outputStream.close();
         }
@@ -89,6 +96,33 @@ public class BasicParser {
             Log.e(LOG_TAG, Log.getStackTraceString(e));
         }
         return null;
+    }
+
+    protected void writeToFile(String input, String fileName) {
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)) {
+            Log.d("Test", "sdcard mounted and writable");
+            File myFile = new File(Environment.getExternalStorageDirectory().getPath() + "/" + fileName);
+            Log.d("test-path", Environment.getExternalStorageDirectory().getPath() + "/" + fileName);
+            try {
+                myFile.createNewFile();
+                FileOutputStream fOut = new FileOutputStream(myFile);
+                OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+                myOutWriter.append(input);
+                myOutWriter.close();
+                fOut.close();
+                Log.d("Test-done", "writen without errors");
+            }
+            catch(IOException e) {
+                Log.e("Test", Log.getStackTraceString(e));
+            }
+        }
+        else if(Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Log.d("Test", "sdcard mounted readonly");
+        }
+        else {
+            Log.d("Test", "sdcard state: " + state);
+        }
     }
 
 }
