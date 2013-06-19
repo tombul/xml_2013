5f2581a63e3e3a8c48d9968edc3199c8b976cbca
Madte <kiafermadte@web.de>
2013-06-19 12:45:21 +0200

    - Hab den Parser jetzt mal umgeschrieben, lokal tut er wieder was er soll. Mit debuggen in Android hab ichs net so raus, Logcat zeigt zig Fehler aber wo steht da der Fehlertext dabei?

--- src/fu/berlin/de/webdatabrowser/webdataparser/LDParser.java
+++ src/fu/berlin/de/webdatabrowser/webdataparser/LDParser.java
@@ -17,16 +17,24 @@ import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.SAXException;
 
 public final class LDParser {
-    
-    static ArrayList<String> tags = new ArrayList<String>( // befüllt mit ein paar Tags die man nehmen könnte
-                                          Arrays.asList("dbpedia-owl:populationTotal", //Einwohnerzahl
-                                                        "dbpedia-owl:leader", // Bürgermeister
-                                                        "geo:geometry", //Koordinaten POINT(13.3989 52.5006)
-                                                        "dbpedia-owl:areaTotal", // Gesamtfläche (xsd:double)
-                                                        "dbpedia-owl:birthPlace", // Geburtsort von...
-                                                        "dbpedia-owl:headquarter", // Hauptquartier von...
-                                                        "dbpedia-owl:hometown" // Heimatstadt von...
-                                                        ));
+
+    static ArrayList<String> tags = new ArrayList<String>( // befüllt mit ein
+                                                           // paar Tags die man
+                                                           // nehmen könnte
+                                  Arrays.asList("dbpedia-owl:populationTotal", // Einwohnerzahl
+                                          "dbpedia-owl:leader", // Bürgermeister
+                                          "geo:geometry", // Koordinaten
+                                                          // POINT(13.3989
+                                                          // 52.5006)
+                                          "dbpedia-owl:areaTotal", // Gesamtfläche
+                                                                   // (xsd:double)
+                                          "dbpedia-owl:birthPlace", // Geburtsort
+                                                                    // von...
+                                          "dbpedia-owl:headquarter", // Hauptquartier
+                                                                     // von...
+                                          "dbpedia-owl:hometown" // Heimatstadt
+                                                                 // von...
+                                  ));
 
     // Gibt dom document als string zurück
     private static String getStringFromDoc(org.w3c.dom.Document doc) {
@@ -45,28 +53,49 @@ public final class LDParser {
         Document targetDoc = db.newDocument();
 
         Element root = targetDoc.createElement("rdf:RDF");
+        root.setAttribute("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        root.setAttribute("xmlns:dbpedia-owl", "http://dbpedia.org/ontology/");
+        root.setAttribute("xmlns:geo", "http://www.w3.org/2003/01/geo/wgs84_pos#");
         targetDoc.appendChild(root);
 
-        boolean importNode;
-
+        // Subjekte die die gewünschten Prädikate enthalten werden in ein neues
+        // Dokument verfrachtet
         for(int i = 0; i < rdfEntries.getLength(); i++) {
-            Node node = rdfEntries.item(i);
-            if(node.getNodeType() == Node.ELEMENT_NODE) {
-                NodeList children = node.getChildNodes();
-                importNode = false;
-                Node copyOfN = targetDoc.importNode(node, false);
-                for(int j = 0; j < children.getLength(); j++) {
-                    Node cNode = children.item(j);
-                    for(String element : LDParser.tags)
-                    {
-                        if(cNode.getNodeName() == element) {
-                            copyOfN.appendChild(targetDoc.importNode(cNode, false));
-                            importNode = true;
+            Node subject = rdfEntries.item(i);
+            if(subject.getNodeType() == Node.ELEMENT_NODE) {
+
+                NodeList predicates = subject.getChildNodes();
+
+                boolean foundTag;
+                for(int j = predicates.getLength() - 1; j >= 0; j--) {
+
+                    Node predicate = predicates.item(j);
+                    foundTag = false;
+
+                    for(String element : tags) {
+                        if(predicate.getNodeName() == element) {
+                            foundTag = true;
                         }
                     }
+
+                    if(!foundTag)
+                        subject.removeChild(predicate); // löscht predicate wenn
+                                                        // es nicht in der
+                                                        // Tagliste gefunden
+                                                        // wurde
                 }
-                if(importNode)
-                    targetDoc.getDocumentElement().appendChild(copyOfN);
+                if(subject.hasChildNodes())
+                    targetDoc.getDocumentElement().appendChild(targetDoc.adoptNode(subject)); // Subjekt
+                                                                                              // wird
+                                                                                              // nur
+                                                                                              // ins
+                                                                                              // neue
+                                                                                              // Doc
+                                                                                              // eingehängt
+                                                                                              // wenn
+                                                                                              // es
+                                                                                              // Prädikate
+                                                                                              // hat
             }
         }
 
