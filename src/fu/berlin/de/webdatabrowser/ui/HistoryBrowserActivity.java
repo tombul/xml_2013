dcf2cb078e63e0b1b4b8e9e09f59df58bdf52709
Julius Auer <julius.auer@gmail.com>
2013-07-03 13:38:56 +0200

    Add progresdialog for queries.

--- src/fu/berlin/de/webdatabrowser/ui/HistoryBrowserActivity.java
+++ src/fu/berlin/de/webdatabrowser/ui/HistoryBrowserActivity.java
@@ -3,6 +3,7 @@ package fu.berlin.de.webdatabrowser.ui;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -38,48 +39,62 @@ public class HistoryBrowserActivity extends Activity {
         final WebView webView = (WebView) findViewById(R.id.historybrowser_webview);
         final ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, PRESET_QUERIES_DESCRIPTION);
         final Spinner spinner = (Spinner) findViewById(R.id.historybrowser_spinner);
+        final ProgressDialog p = new ProgressDialog(this);
+        p.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        p.setMessage("processing ...");
         spinnerAdapter.setDropDownViewResource(R.layout.spinner_item);
         spinner.setAdapter(spinnerAdapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             int lastSelected = 0;
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, final int position, long id) {
                 if(lastSelected == position)
                     return;
 
                 lastSelected = position;
+                p.show();
 
                 if(position == 0) {
                     new AsyncTask<String, Void, Boolean>() {
 
                         @Override
                         protected Boolean doInBackground(String... params) {
-                            return rdfStore.writeQueryFormattedResultToFile(PRESET_QUERIES[0], "rdfstore_dump.txt");
+                            boolean result = rdfStore.writeQueryFormattedResultToFile(PRESET_QUERIES[0], "rdfstore_dump.txt");
+                            p.dismiss();
+                            return result;
                         }
                     }.execute();
 
                     return;
                 }
 
-                List<Object> resources = rdfStore.performQuery(PRESET_QUERIES[position]);
-                String source = "<!DOCTYPE html><html>";
+                new AsyncTask<String, Void, Void>() {
 
-                if(resources.isEmpty()) {
-                    source += "Nothing useful found.";
-                }
-                else if(resources.get(0) != null && !(resources.get(0) instanceof Literal)) {
-                    source += ((DeebResource) resources.get(0)).getHeaderHtml();
-                }
+                    @Override
+                    protected Void doInBackground(String... params) {
+                        List<Object> resources = rdfStore.performQuery(PRESET_QUERIES[position]);
+                        String source = "<!DOCTYPE html><html>";
 
-                for(Object resource : resources)
-                    if(resource instanceof Literal)
-                        source += ((Literal) resource).getLexicalForm() + "<p/>";
-                    else
-                        source += ((DeebResource) resource).getHtml() + "<p/>";
+                        if(resources.isEmpty()) {
+                            source += "Nothing useful found.";
+                        }
+                        else if(resources.get(0) != null && !(resources.get(0) instanceof Literal)) {
+                            source += ((DeebResource) resources.get(0)).getHeaderHtml();
+                        }
 
-                source += "</html>";
-                webView.loadDataWithBaseURL(null, source, "text/html", "UTF-8", null);
+                        for(Object resource : resources)
+                            if(resource instanceof Literal)
+                                source += ((Literal) resource).getLexicalForm() + "<p/>";
+                            else
+                                source += ((DeebResource) resource).getHtml() + "<p/>";
+
+                        source += "</html>";
+                        p.dismiss();
+                        webView.loadDataWithBaseURL(null, source, "text/html", "UTF-8", null);
+                        return null;
+                    }
+                }.execute();
             }
 
             @Override
