66a9501e6cc17f1bd94e389e2e8364e1dc470219
Julius Auer <julius.auer@gmail.com>
2013-07-03 11:03:55 +0200

    Select distinct.

--- src/fu/berlin/de/webdatabrowser/ui/HistoryBrowserActivity.java
+++ src/fu/berlin/de/webdatabrowser/ui/HistoryBrowserActivity.java
@@ -22,7 +22,7 @@ import fu.berlin.de.webdatabrowser.ui.widgets.MenuItem;
 public class HistoryBrowserActivity extends Activity {
     protected static final String[] PRESET_QUERIES             = new String[] {
                                                                "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object }",
-                                                               "SELECT ?object WHERE { ?subject <http://Schema.org/author> ?object }" };
+                                                               "SELECT DISTINCT ?object WHERE { ?subject <http://Schema.org/author> ?object }",
                                                                "SELECT DISTINCT ?object WHERE { ?subject <http://purl.org/dc/elements/1.1/subject> ?object }" };
     private static final String[]   PRESET_QUERIES_DESCRIPTION = new String[] {
                                                                "everything (file)",
